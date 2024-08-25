767182bd638d4142b5d342ab8083920cb1da0110
Kyrylo Zharenkov <kyrylo.zharenkov@gmail.com>
2024-08-25 17:57:57 +0200

    introduce clang-format

--- request.h
+++ request.h
@@ -1,34 +1,41 @@
 #pragma once
+
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
-namespace Agregator {
-    enum class RequestType : uint8_t {
-     sel,
-     cnt,
-     err
-    };
+namespace Aggregator
+{
 
-    struct SelRequest {
-        int event_id;
-        std::string uuid;
-        std::string banner_id;
-        double price;
-    };
+enum class RequestType : uint8_t
+{
+    sel,
+    cnt,
+    err
+};
 
-    struct CntRequest {
-        int event_id;
-        std::string sel_request_uuid;
-    };
+struct SelRequest final
+{
+    int event_id;
+    std::string uuid;
+    std::string banner_id;
+    double price;
+};
 
-    using SelRequestsStorage = std::unordered_map<std::string, SelRequest>;
-    using CntRequestsStorage = std::vector<CntRequest>;
+struct CntRequest final
+{
+    int event_id;
+    std::string sel_request_uuid;
+};
 
-    using Events = std::unordered_set<int>;
-    using BannerRequests = std::unordered_map<std::string, Events>;
-    using Banners = std::unordered_map<std::string, BannerRequests>;
+using SelRequestsStorage = std::unordered_map<std::string, SelRequest>;
+using CntRequestsStorage = std::vector<CntRequest>;
 
-    using EventsAmount = std::unordered_map<std::string, std::unordered_map<int,int>>;
-    using BannersPrices = std::unordered_map<std::string, double>;
+using Events = std::unordered_set<int>;
+using BannerRequests = std::unordered_map<std::string, Events>;
+using Banners = std::unordered_map<std::string, BannerRequests>;
+
+using EventsAmount = std::unordered_map<std::string, std::unordered_map<int,int>>;
+using BannersPrices = std::unordered_map<std::string, double>;
+
 }
